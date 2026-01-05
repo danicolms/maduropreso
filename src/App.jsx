89e9bb7b212439833b3f6233e5a777c292a147b8
Daniel Colmenares Salamanca <hello@danicolms.xyz>
2026-01-05 01:18:39 +0100

    como termine aqui WIP + hero/header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,14 @@
-import "./styles.css";
+import "./styles.css"
+import Hero from "./components/Hero"
+import Header from "./components/Header"
 
 function App() {
   return (
-    <div className="hero-section">
-      <img
-        src="/hero-paper.png"
-        alt="Paper background"
-        className="hero-paper"
-      />
-      <div className="hero-content">
-        <img src="/logo.png" alt="maduropreso" className="hero-logo" />
-        <p className="hero-text">
-          PRIMER BLOG DEDICADO A LA CAPTURA Y APRESAMIENTO DE NICOLAS MADURO
-        </p>
-      </div>
+    <div className="main-container">
+      <Hero />
+      <Header />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Hero.jsx
@@ -0,0 +1,19 @@
+function Hero() {
+  return (
+    <div className="hero-section">
+      <img
+        src="/hero-paper.png"
+        alt="Paper background"
+        className="hero-paper"
+      />
+      <div className="hero-content">
+        <img src="/logo.png" alt="maduropreso" className="hero-logo" />
+        <p className="hero-text">
+          PRIMER BLOG DEDICADO A LA CAPTURA Y APRESAMIENTO DE NICOLAS MADURO
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
