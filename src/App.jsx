da529618decfea80ffa1f52993220304fce5f31f
Daniel Colmenares Salamanca <hello@danicolms.xyz>
2026-01-05 01:23:16 +0100

    repository comment for devs

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import "./styles.css"
-import Hero from "./components/Hero"
-import Header from "./components/Header"
+import "./styles.css";
+import Hero from "./components/Hero";
+import Header from "./components/Header";
 
 function App() {
+  console.log(
+    "Are you one of the tech fellows? This project's repository is public for all to see! ----> https://github.com/danicolms/maduropreso✨",
+  );
   return (
     <div className="main-container">
       <Hero />
       <Header />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
